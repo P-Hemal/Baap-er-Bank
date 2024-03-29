6c7c1aacb083efde06d5d0745596a4a85314291f
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-29 15:34:38 +0600

    deposit

--- js/login.js
+++ js/login.js
@@ -3,6 +3,12 @@ document.getElementById('btn-submit').addEventListener('click', function(){
     const email = emailField.value;
     const passwordField = document.getElementById('user-password');
     const password = passwordField.value;
+    if(email === 'abdulhakim.hemal.99@gmail.com' && password === 'alhamdulillah'){
+        window.location.href = 'bank.html';
+    }
+    else{
+        alert('You can not get enter into the next page')
+    }
     
 })
 
