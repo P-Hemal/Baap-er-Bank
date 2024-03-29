fac9ff0b5fe75eee3be3ce424e956a2c35833b47
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-29 16:53:28 +0600

    balance

--- js/deposit.js
+++ js/deposit.js
@@ -1,1 +1,21 @@
-document.getElementById
+document.getElementById('btn-deposit').addEventListener('click', function () {
+    const depositField = document.getElementById('deposit-field');
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+    depositTotalElement.innerText = currentDepositTotal;
+
+
+    depositField.value = '';
+})
