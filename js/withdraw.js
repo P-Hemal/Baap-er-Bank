fd2de31824eb1656b95fec45ec126b0ec88e75b7
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-30 14:03:02 +0600

    withdraw

---
+++ js/withdraw.js
@@ -0,0 +1,12 @@
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.innerText;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+})
